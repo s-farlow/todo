0b93ed94404be174923b10e538039c5bf3f449ef
s-farlow <sfarlow@arizona.edu>
2026-04-21 13:41:28 -0700

    allow for phone access to localhost

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0", // maybe take out for deployment
     proxy: {
       "/api": "http://localhost:3001",
     },
